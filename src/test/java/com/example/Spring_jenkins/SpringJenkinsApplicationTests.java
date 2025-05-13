2ad71e819256e9c5ef1d399630d22bbdea331950
shiva <sivasunke7895@gmail.com>
2025-05-13 15:36:13 +0530

    first commit

---
+++ src/test/java/com/example/Spring_jenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.Spring_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	Logger log = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+	@Test
+	public void contextLoads() {
+		log.info("Test case is Executed..!");
+		assertEquals(true,true);
+	}
+
+}
